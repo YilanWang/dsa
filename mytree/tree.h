24cb99e899ea87c3f3af0f697e22903a129850c1
wang yilan <wangyilan93@126.com>
2017-10-05 22:59:20 +0800

    binTree

---
+++ mytree/tree.h
@@ -0,0 +1,99 @@
+//
+// Created by alan-wang on 17-10-5.
+//
+//#pragma once
+
+
+#ifndef MYTREE_TREE_H
+#define MYTREE_TREE_H
+
+
+#include <cstdlib>
+#include "STACK.h"
+
+#define BinNodePos(T) BinNode<T>*   //节点位置
+#define stature(p) ((p)?(p)->height:-1)
+typedef enum {
+    RB_RED, RB_BLACK
+} RBColor;
+
+template<typename T>
+struct BinNode {
+
+    //成员
+    T data;
+    BinNodePos(T)parent;
+    BinNodePos(T)lc;
+    BinNodePos(T)rc;
+    int height;
+    int npl;
+    RBColor color;
+
+    //构造函数
+    BinNode() :
+            parent(nullptr), lc(nullptr), rc(nullptr), height(0), npl(1), color(RB_RED) {}
+
+    BinNode(T e, BinNodePos(T)p = nullptr, BinNodePos(T)lc = nullptr, BinNodePos(T)rc = nullptr, int h = 0, int l = 1,
+            RBColor c = RB_RED) :
+            data(e), parent(p), lc(lc), rc(rc), height(h), npl(l), color(c) {}
+
+    //操作API
+    int size();
+
+    BinNodePos(T)insertAsLC(T const &);
+
+    BinNodePos(T)insertAsRC(T const &);
+
+    BinNodePos(T)succ();
+
+    template<typename VST>
+    void traLevel(VST &); //子树层次遍历
+    template<typename VST>
+    void travPre(VST &);  //先序遍历
+    template<typename VST>
+    void travIn(VST &);   //中序遍历
+    template<typename VST>
+    void travPost(VST &); //后序遍历
+
+    //操作符ETC
+    bool operator<(BinNode const &bn) { return data < bn.data; }
+
+    bool operator==(BinNode const &bn) { return data == bn.data; }
+};
+#define IsRoot(x) (!((x).parent))
+#define IsLChild(x) (!IsRoot(x) && (&(x)==(x).parent->lc))
+#define IsRChild(x) (!IsRoot(x) && (&(x)==(x).parent->rc))
+#define HasParent(x) (!IsRoot(x))
+#define HsaLChild(x) ((x).lc)
+#define HasRChild(x) ((x).rc)
+#define HasChild(x) (HasLChild(x) || HasRchild(x))
+#define HasBothChild(x) (HasLChild(x) && HasRchild(x))
+#define IsLeaf(x) (!HasChild(x))
+#define sibling(p) (IsLChild(*(p))?(p)->parent->rc:(p)->parent->lc)
+#define uncle(x) (IsLChild(*((x)->parent))?(x)->parent->parent->rc:(x)->parent->parent->lc)
+#define FromParentTo(x) (IsRoot(x)?_root:(IsLChild(x)?(x).parent->lc:(x).parent->rc))
+
+
+template <typename T>BinNodePos(T) BinNode<T>::insertAsLC(T const &e) {
+    return lc=new BinNode(e,this);
+}
+
+template <typename T>BinNodePos(T) BinNode<T>::insertAsRC(T const &e) {
+    return rc=new BinNode(e,this);
+}
+
+template <typename T>template <typename VST>
+void BinNode<T>::travIn(VST & visit) {
+    switch (rand()%5){
+        case 1:travIn_I1(this,visit);
+            break;
+        case 2:travIn_I2(this,visit);
+            break;
+        case 3:travIn_I3(this,visit);
+            break;
+        case 4:travIn_I4(this,visit);
+            break;
+        default:travIn_R(this,visit);
+    }
+}
+#endif //MYTREE_TREE_H
